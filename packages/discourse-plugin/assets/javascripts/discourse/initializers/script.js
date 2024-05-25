280623bf297841d7db939ca0d261424cb44d87c8
morrigan <belma.dzi@gmail.com>
2024-05-25 17:53:34 +0200

    add pcd-zuauth

--- packages/discourse-plugin/assets/javascripts/discourse/initializers/script.js
+++ packages/discourse-plugin/assets/javascripts/discourse/initializers/script.js
@@ -1,20 +1,63 @@
 import { withPluginApi } from 'discourse/lib/plugin-api';
+import zuauth from "../lib/pcd-zuauth";
+
+const fieldsToReveal = {
+  revealTicketId: true,
+  revealEventId: true,
+  revealAttendeeSemaphoreId: true,
+  revealProductId: true,
+};
 
 export default {
     name: 'listener',
     initialize() {
       withPluginApi('0.8.9', api => {
         api.onPageChange(() => {
-          const loginButton = document.querySelector(".btn.oauth2_basic");
-        
-            if (loginButton) {
-              alert("WOWOWOW");
-              loginButton.addEventListener("click", clickCallback, { once: true });
+          const observeLoginButton = () => {
+            // Function to handle the login click event
+            const handleLoginButtonClick = async(event) => {
+              event.preventDefault();
+              console.log('Custom login action!');
+
+              try {
+                const result = await zuauth.zuAuthPopup({
+                  zupassUrl: process.env.NEXT_PUBLIC_ZUPASS_SERVER_URL,
+                  fieldsToReveal,
+                  watermark: '11114r50',
+                  config: zuauth.ETHBERLIN04
+                });
+                console.log(result);
+              } catch (e) {
+                console.error(e);
+              }
+              
+            };
+  
+            // Create a mutation observer to watch for changes in the modal container
+            const modalContainer = document.querySelector('.modal-container');
+            if (modalContainer) {
+              const observer = new MutationObserver((mutations) => {
+                mutations.forEach((mutation) => {
+                  if (mutation.addedNodes.length) {
+                    // Check if the login button is present in the added nodes
+                    const loginButton = modalContainer.querySelector('.btn.oauth2_basic');
+                    if (loginButton) {
+                      // Attach the event listener to the login button
+                      loginButton.addEventListener('click', handleLoginButtonClick);
+                      // Once found and listener attached, disconnect the observer
+                      observer.disconnect();
+                    }
+                  }
+                });
+              });
+  
+              // Start observing the modal container for child list changes
+              observer.observe(modalContainer, { childList: true, subtree: true });
             }
-        //   api.onAppEvent("modal:body-shown", (event) => {
-        //     alert("HEY");
-        //     
-        //   });
+          };
+  
+          // Call the function to set up the observer
+          observeLoginButton();
         });
       });
     }
